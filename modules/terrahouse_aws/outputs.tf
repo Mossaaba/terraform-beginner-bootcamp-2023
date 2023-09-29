86b07845ceb302a8233a98c3e52240c8ea64b5ab
Mouss AID <m_aidoud@esi.dz>
2023-09-29 21:13:21 +0200

    #28 enabled static website hosting, uploaded index and error file (#29)

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,3 +1,26 @@
 output "bucket_name" {
   value = aws_s3_bucket.website_bucket.bucket
 }
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
+
+variable "index_html_filepath" {
+  description = "The file path for index.html"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exist."
+  }
+}
+
+variable "error_html_filepath" {
+  description = "The file path for error.html"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The provided path for error.html does not exist."
+  }
+}
